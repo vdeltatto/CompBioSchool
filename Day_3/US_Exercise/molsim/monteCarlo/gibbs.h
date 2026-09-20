bd1b88bc1ae36cf4612479acae17b80c8ec10208
Vittorio Del Tatto <vdeltatt@sissa.it>
2026-09-21 00:59:08 +0530

    day1 final version

--- Day_3/US_Exercise/molsim/monteCarlo/gibbs.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "mc.h"
-
-void gibbsVolumeMove(MonteCarlo& systemA, MonteCarlo& systemB);
-void gibbsSwapMove(MonteCarlo& systemA, MonteCarlo& systemB);
-void runGibbsEnsemble(MonteCarlo& systemA, MonteCarlo& systemB);
